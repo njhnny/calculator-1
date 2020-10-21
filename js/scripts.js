d315ad0931b544097edf4caf3fb752e854c329b0
Nick Hennessy <njhnny@gmail.com>
2020-10-21 15:13:49 -0700

    add functions for different math operations
    Co-authored-by: Zahnen Garner <zahnen@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
